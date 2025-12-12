129b534cceaca4b28df997e456d65844678fe6fb
John Cuvi <vladimir@coopdaquilema.com>
2025-12-12 17:06:10 -0500

    First commit for Restaurant funny GRAPH QL

---
+++ src/main/java/com/ithink/ResturantfunnyApplication.java
@@ -0,0 +1,13 @@
+package com.ithink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResturantfunnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResturantfunnyApplication.class, args);
+	}
+
+}
